afbd247917f56344c44c3d60db46547696632335
John Bjerke <bjerkeweb@gmail.com>
2017-04-14 16:53:09 -0400

    add react-waypoint and onScreen state

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
+import Waypoint from 'react-waypoint';
 
 class Image extends Component {
 
   constructor() {
     super();
 
+    this.onLoad = this.onLoad.bind(this);
+    this.toggleOnScreen = this.toggleOnScreen.bind(this);
+
     this.state = {
-      imageLoaded: false,
+      isLoaded: false,
       onScreen: false
     }
+  }
+
+  onLoad() {
+    this.setState({ isLoaded: true });
+  }
+
+  toggleOnScreen() {
+    this.setState({ onScreen: !this.state.onScreen });
   }
 
   render() {
     const url = `./images/${this.props.name}`;
     const imageName = this.props.name.split('.')[0];
+
+    const imgClass = [
+      ( this.state.onScreen )
+        ? 'is-loaded'
+        : 'is-not-loaded',
+      'image__img'
+    ];
+
     return (
       <div id={imageName} className="pane pane--image">
+        <Waypoint
+          key={imageName}
+          horizontal={true}
+          topOffset="-50%"
+          bottomOffset="0"
+          onEnter={ this.toggleOnScreen }
+          onLeave={ this.toggleOnScreen }
+        />
         <img
           style={{ minWidth: `calc( (100vh - 7.5rem) * ${this.props.aspectRatio} )` }}
+          className={ imgClass.join(' ') }
           src={url}
         />
         <div className="image-metadata">
